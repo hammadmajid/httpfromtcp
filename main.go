ff1ed3991711e0d0fd90dfe41a07b5b511bcac07
Hammad Majid <hammadmajid@proton.me>
2025-11-17 03:03:52 +0500

    read line but 8 bytes at a time

--- main.go
+++ main.go
@@ -2,23 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 func main() {
 	file, err := os.Open("./messages.txt")
 	if err != nil {
-		println("Failed to open file")
-		os.Exit(1)
+		panic(err)
 	}
+	defer file.Close()
+
+	var line string
 
 	for {
+		var current string
+
 		var bytes [8]byte
 		_, err := file.Read(bytes[:])
-		if err != nil {
-			os.Exit(0)
+		if err == io.EOF {
+			break
 		}
 
-		fmt.Printf("read: %s\n", bytes)
+		current = string(bytes[:])
+		parts := strings.Split(current, "\n")
+
+		if len(parts) != 1 { // found \n
+			line += parts[0]
+			fmt.Printf("read: %s\n", line)
+
+			line = parts[1]
+		} else {
+			line += current
+		}
 	}
+
 }
